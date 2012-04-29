ac67b0f3ee8ac8583636fcea3ab95acef7f874b9
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-04-29 13:19:00 +0200

    Fixed routing error for medical aids and their plans

--- app/models/person_medical_aid.rb
+++ app/models/person_medical_aid.rb
@@ -1,7 +1,7 @@
 class PersonMedicalAid < ActiveRecord::Base
-  attr_accessible :person_id, :aid_number, :medical_aid_id, :status, :joinDate
+  attr_accessible :person_id, :aid_number, :medical_aids_id, :status, :join_date
 
   belongs_to :person, :foreign_key => :id
-  has_one :medical_aid_plan, :foreign_key => :id
+  has_one  :medical_aid_plan, :foreign_key => :id,:primary_key => :medical_aids_id
 
 end
