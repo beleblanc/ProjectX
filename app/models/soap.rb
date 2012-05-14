d8c201b1136ef9baeb5f5c89871163d233a04faf
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-05-14 09:12:58 +0200

    Added Vitals

--- app/models/soap.rb
+++ app/models/soap.rb
@@ -1,5 +1,5 @@
 class Soap < ActiveRecord::Base
-  attr_accessible :comment, :consultation_id, :soap_type_id, :user_id
+  attr_accessible :comment, :consultation_id, :soap_type_id, :user_id, :vitals_attributes
 
   has_many :vitals
   has_many :orders
@@ -7,5 +7,7 @@ class Soap < ActiveRecord::Base
 
   belongs_to :consultation
   belongs_to :soap_type
+
+  accepts_nested_attributes_for :vitals
 
 end
