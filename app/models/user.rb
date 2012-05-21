a841024634b6214fc41bc25bdc97853729ac8463
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-05-21 10:00:37 +0200

    Fixed role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  #resourcify
-
+  resourcify
+  rolify
   has_and_belongs_to_many :roles, :join_table => :users_roles
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
