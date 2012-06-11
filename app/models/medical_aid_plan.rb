3f680a6568c5f42de502e526f065e8ebf1d322d2
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-06-11 11:37:55 +0200

    Starting pricing for orders

--- app/models/medical_aid_plan.rb
+++ app/models/medical_aid_plan.rb
@@ -1,5 +1,5 @@
 class MedicalAidPlan < ActiveRecord::Base
-  attr_accessible  :plan_name, :medical_aids_id
+  attr_accessible  :name, :medical_aid_id
 
   belongs_to :medical_aid
   has_many :person_medical_aids
