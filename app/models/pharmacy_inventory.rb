741fdadd1ad2e46236102adb794d5a75683b9872
Tewodros Asrat <teddya2007@gmail.com>
2012-06-28 22:40:47 +0200

    Pharmacy Inventory Prescription

--- app/models/pharmacy_inventory.rb
+++ app/models/pharmacy_inventory.rb
@@ -1,5 +1,6 @@
 class PharmacyInventory < ActiveRecord::Base
   attr_accessible :description, :expiry_date, :name, :reorder_level, :stock_on_hand, :prices_attributes
   has_many :prices, as: :priceable
+  has_many :prescriptions
   accepts_nested_attributes_for :prices, :allow_destroy => true
 end
