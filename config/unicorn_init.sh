bfab995a45c2c66c53387a4c4cb9cb7ebf99ff71
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-06-29 15:00:18 +0200

    Final push before presentation

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/passenger/apps/ProjectX/current
+APP_ROOT=/home/teddy/apps/ProjectX/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=passenger
+AS_USER=teddy
 set -u
 
 OLD_PIN="$PID.oldbin"
