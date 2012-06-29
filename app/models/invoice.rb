b023339e78794fecfc188c06aee9a80ab5c8d072
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-06-29 14:47:46 +0200

    Final push before presentation

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -24,7 +24,7 @@ class Invoice < ActiveRecord::Base
   end
  
   def build_prescription_items
-    self.consulation.prescriptions.all.each do |prescription|
+    self.consultation.prescriptions.all.each do |prescription|
 	line = {:quantity => 1.0, :unit_cost => prescription.price, :total => prescription.price, :description => prescription.pharmacy_inventory.description}
 	self.line_items.build(line)
     end
